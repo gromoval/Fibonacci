cc04957dd803b4150e0b8592928131534899beb7
danilishe <yraiy-45674>
2019-12-09 20:57:01 +0300

    Добавлен логгер и несколько тестов

--- src/main/java/org/example/FibCalc.java
+++ src/main/java/org/example/FibCalc.java
@@ -1,7 +1,20 @@
 package org.example;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class FibCalc {
+    private Logger log = LogManager.getLogger();
+
     public long calc(long i) {
+        log.trace("Считаем число Фибоначчи: {}", i);
+/*        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            log.error(e);
+        }*/
+        if (i < 0) throw new IllegalArgumentException("Мы не считаем значения меньше 0!");
         if (i == 0) return 0;
         if (i == 1) return 1;
         return calc(i - 1) + calc(i - 2);
--- src/test/java/org/example/TestFib.java
+++ src/test/java/org/example/TestFib.java
@@ -1,13 +1,41 @@
 package org.example;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class TestFib {
-    @Test
-    public void testFib() {
-        FibCalc fibCalc = new FibCalc();
-        Assert.assertEquals(fibCalc.calc(9), 34, "Неправильный ответ");
+    private Logger log = LogManager.getLogger();
+    private FibCalc fibCalc;
+
+    @DataProvider(parallel = true)
+    public static Object[][] data() {
+        return new Object[][]{
+                {0, 0},
+                {1, 1},
+                {10, 55},
+                {9, 34}
+        };
+    }
+
+    @Test(dataProvider = "data")
+    public void testFib(long n, long exp) {
+        long calc = fibCalc.calc(n);
+        log.info("Получено число {}", calc);
+        Assert.assertEquals(calc, exp, "Неправильный ответ");
+    }
+
+    @BeforeClass
+    public void setUp() {
+        fibCalc = new FibCalc();
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void expectExceptionWhenNegative() {
+        fibCalc.calc(-10);
     }
 }
